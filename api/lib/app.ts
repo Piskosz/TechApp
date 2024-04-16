fdf92fbd50cdd9a90b2e76dd4251f27205c08cc2
Piskosz <132614400+Piskosz@users.noreply.github.com>
2024-04-16 19:09:18 +0200

    Mean-app
    Mean-app

---
+++ api/lib/app.ts
@@ -0,0 +1,25 @@
+import express from 'express';
+import { config } from './config';
+import Controller from 'interface/controller.interface';
+
+class App {
+    public app: express.Application;
+
+   constructor(controllers: Controller[]) {
+       this.app = express();
+       this.initializeControllers(controllers);
+   }
+
+   private initializeControllers(controllers: Controller[]): void {
+       controllers.forEach((controller) => {
+           this.app.use('/', controller.router);
+       });
+   }
+
+   public listen(): void {
+       this.app.listen(config.port, () => {
+           console.log(`App listening on the port ${config.port}`);
+       });
+   }
+}
+export default App;
